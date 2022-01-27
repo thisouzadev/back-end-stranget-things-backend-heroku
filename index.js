50344b89c6d7016b55222d2f98ec1a3cd38c6ace
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-01-27 17:06:34 -0300

    requisito 6

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const { PORT, UPSIDEDOWN_MODE } = process.env;
+const { UPSIDEDOWN_MODE } = process.env;
+const PORT = process.env.PORT || 3000;
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
